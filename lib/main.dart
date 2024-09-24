00af8dea528058b26c6e99b52008f91d773a45c7
masaPokemon <masa.gongon2011@gmail.com>
2024-09-24 11:27:02 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
- 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MaterialApp(home: MyApp()));
-}
- 
-class MyApp extends StatefulWidget {
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+import 'package:fwfh_webview/fwfh_webview.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
- 
-class _MyAppState extends State<MyApp> {
-  final GlobalKey webViewKey = GlobalKey();
- 
-  InAppWebViewController? webViewController;
- 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Mochoto InAppWebView")),
-      body: SafeArea(
-          child: InAppWebView(
-        key: webViewKey,
-        initialUrlRequest: URLRequest(url: WebUri("https://inappwebview.dev/")),
-        onWebViewCreated: (controller) {
-          webViewController = controller;
-        },
-      )),
+    return MaterialApp(
+      title: 'fwfh_webview',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('WebViewFactory Demo'),
+        ),
+        body: Center(
+          child: HtmlWidget(
+            '<iframe src="https://www.youtube.com/embed/jNQXAC9IVRw"></iframe>',
+            factoryBuilder: () => MyWidgetFactory(),
+          ),
+        ),
+      ),
     );
   }
 }
- 
+
+class MyWidgetFactory extends WidgetFactory with WebViewFactory {}
