fafc2c2ead012afbb6bcb4eb45e07908ec0f0ddb
masaPokemon <masa.gongon2011@gmail.com>
2024-09-24 10:36:33 +0900

    Update main.darts

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+ 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(home: MyApp()));
 }
-
-class MyApp extends StatelessWidget {
+ 
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
-
+ 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MyHomePage(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
+ 
+class _MyAppState extends State<MyApp> {
+  final GlobalKey webViewKey = GlobalKey();
+ 
+  InAppWebViewController? webViewController;
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WebViewWidget(
-        controller: WebViewController()
-          ..loadRequest(Uri.parse("https://www.google.com/search?q=%E3%83%9D%E3%82%B1%E3%82%AB&rlz=1C5CHFA_enJP1103JP1105&oq=%E3%83%9D%E3%82%B1%E3%82%AB&gs_lcrp=EgZjaHJvbWUqCQgAEEUYOxiABDIJCAAQRRg7GIAEMg0IARAAGIMBGLEDGIAEMg0IAhAAGIMBGLEDGIAEMgoIAxAAGLEDGIAEMhAIBBAAGIMBGLEDGIAEGIoFMg0IBRAAGIMBGLEDGIAEMgYIBhBFGD0yBggHEEUYPdIBCDI1NTdqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8")),
-      ),
+      appBar: AppBar(title: const Text("Mochoto InAppWebView")),
+      body: SafeArea(
+          child: InAppWebView(
+        key: webViewKey,
+        initialUrlRequest: URLRequest(url: WebUri("https://inappwebview.dev/")),
+        onWebViewCreated: (controller) {
+          webViewController = controller;
+        },
+      )),
     );
   }
 }
+ 
