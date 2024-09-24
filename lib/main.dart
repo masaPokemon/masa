67782ef8f87fba19c89c1e70e02c504cb04b5620
masaPokemon <masa.gongon2011@gmail.com>
2024-09-24 13:24:47 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  }
 
   runApp(const MaterialApp(home: MyApp()));
 }
@@ -17,13 +21,111 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
+  HeadlessInAppWebView? headlessWebView;
+  String url = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    headlessWebView = HeadlessInAppWebView(
+      initialUrlRequest: URLRequest(url: WebUri("https://github.com/flutter")),
+      initialSettings: InAppWebViewSettings(isInspectable: kDebugMode),
+      onWebViewCreated: (controller) {
+        const snackBar = SnackBar(
+          content: Text('HeadlessInAppWebView created!'),
+          duration: Duration(seconds: 1),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      onConsoleMessage: (controller, consoleMessage) {
+        final snackBar = SnackBar(
+          content: Text('Console Message: ${consoleMessage.message}'),
+          duration: const Duration(seconds: 1),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      onLoadStart: (controller, url) async {
+        final snackBar = SnackBar(
+          content: Text('onLoadStart $url'),
+          duration: const Duration(seconds: 1),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+        setState(() {
+          this.url = url?.toString() ?? '';
+        });
+      },
+      onLoadStop: (controller, url) async {
+        final snackBar = SnackBar(
+          content: Text('onLoadStop $url'),
+          duration: const Duration(seconds: 1),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+        setState(() {
+          this.url = url?.toString() ?? '';
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    headlessWebView?.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(url: WebUri("https://flutter.dev")),
-      )
-    );
+        appBar: AppBar(
+            title: const Text(
+          "HeadlessInAppWebView Example",
+        )),
+        body: SafeArea(
+            child: Column(children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+                "URL: ${(url.length > 50) ? "${url.substring(0, 50)}..." : url}"),
+          ),
+          Center(
+            child: ElevatedButton(
+                onPressed: () async {
+                  await headlessWebView?.dispose();
+                  await headlessWebView?.run();
+                },
+                child: const Text("Run HeadlessInAppWebView")),
+          ),
+          Center(
+            child: ElevatedButton(
+                onPressed: () async {
+                  if (headlessWebView?.isRunning() ?? false) {
+                    await headlessWebView?.webViewController
+                        ?.evaluateJavascript(
+                            source: "console.log('Here is the message!');");
+                  } else {
+                    const snackBar = SnackBar(
+                      content: Text(
+                          'HeadlessInAppWebView is not running. Click on "Run HeadlessInAppWebView"!'),
+                      duration: Duration(milliseconds: 1500),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                },
+                child: const Text("Send console.log message")),
+          ),
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  headlessWebView?.dispose();
+                  setState(() {
+                    url = '';
+                  });
+                },
+                child: const Text("Dispose HeadlessInAppWebView")),
+          )
+        ])));
   }
 }
