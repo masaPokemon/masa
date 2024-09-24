3233f41b61c55b32cb7f6bcb1fd17fb09db99fb3
masaPokemon <masa.gongon2011@gmail.com>
2024-09-24 12:40:35 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:core';
+import 'package:http/http.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
