8ec83d94853163fccd4ce894f2ebca5560265c69
masaPokemon <masa.gongon2011@gmail.com>
2024-09-24 13:31:26 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,89 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
 import 'dart:async';
-import 'package:flutter/foundation.dart';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
+import 'package:webview_flutter_web/webview_flutter_web.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
-    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
-  }
-
-  runApp(const MaterialApp(home: MyApp()));
+void main() {
+  WebViewPlatform.instance = WebWebViewPlatform();
+  runApp(const MaterialApp(home: _WebViewExample()));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class _WebViewExample extends StatefulWidget {
+  const _WebViewExample();
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _WebViewExampleState createState() => _WebViewExampleState();
 }
 
-class _MyAppState extends State<MyApp> {
-  HeadlessInAppWebView? headlessWebView;
-  String url = "";
-
-  @override
-  void initState() {
-    super.initState();
-
-    headlessWebView = HeadlessInAppWebView(
-      initialUrlRequest: URLRequest(url: WebUri("https://github.com/flutter")),
-      initialSettings: InAppWebViewSettings(isInspectable: kDebugMode),
-      onWebViewCreated: (controller) {
-        const snackBar = SnackBar(
-          content: Text('HeadlessInAppWebView created!'),
-          duration: Duration(seconds: 1),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      },
-      onConsoleMessage: (controller, consoleMessage) {
-        final snackBar = SnackBar(
-          content: Text('Console Message: ${consoleMessage.message}'),
-          duration: const Duration(seconds: 1),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      },
-      onLoadStart: (controller, url) async {
-        final snackBar = SnackBar(
-          content: Text('onLoadStart $url'),
-          duration: const Duration(seconds: 1),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-        setState(() {
-          this.url = url?.toString() ?? '';
-        });
-      },
-      onLoadStop: (controller, url) async {
-        final snackBar = SnackBar(
-          content: Text('onLoadStop $url'),
-          duration: const Duration(seconds: 1),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-        setState(() {
-          this.url = url?.toString() ?? '';
-        });
-      },
+class _WebViewExampleState extends State<_WebViewExample> {
+  final PlatformWebViewController _controller = PlatformWebViewController(
+    const PlatformWebViewControllerCreationParams(),
+  )..loadRequest(
+      LoadRequestParams(
+        uri: Uri.parse('https://flutter.dev'),
+      ),
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    headlessWebView?.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text(
-          "HeadlessInAppWebView Example",
-        )),
-        body: SafeArea(
-            child: Column(children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-                "URL: ${(url.length > 50) ? "${url.substring(0, 50)}..." : url}"),
-          ),
-          Center(
-            child: ElevatedButton(
-                onPressed: () async {
-                  await headlessWebView?.dispose();
-                  await headlessWebView?.run();
-                },
-                child: const Text("Run HeadlessInAppWebView")),
-          ),
-          Center(
-            child: ElevatedButton(
-                onPressed: () async {
-                  if (headlessWebView?.isRunning() ?? false) {
-                    await headlessWebView?.webViewController
-                        ?.evaluateJavascript(
-                            source: "console.log('Here is the message!');");
-                  } else {
-                    const snackBar = SnackBar(
-                      content: Text(
-                          'HeadlessInAppWebView is not running. Click on "Run HeadlessInAppWebView"!'),
-                      duration: Duration(milliseconds: 1500),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  }
-                },
-                child: const Text("Send console.log message")),
-          ),
-          Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  headlessWebView?.dispose();
-                  setState(() {
-                    url = '';
-                  });
-                },
-                child: const Text("Dispose HeadlessInAppWebView")),
-          )
-        ])));
+      appBar: AppBar(
+        title: const Text('Flutter WebView example'),
+        actions: <Widget>[
+          _SampleMenu(_controller),
+        ],
+      ),
+      body: PlatformWebViewWidget(
+        PlatformWebViewWidgetCreationParams(controller: _controller),
+      ).build(context),
+    );
+  }
+}
+
+enum _MenuOptions {
+  doPostRequest,
+}
+
+class _SampleMenu extends StatelessWidget {
+  const _SampleMenu(this.controller);
+
+  final PlatformWebViewController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<_MenuOptions>(
+      onSelected: (_MenuOptions value) {
+        switch (value) {
+          case _MenuOptions.doPostRequest:
+            _onDoPostRequest(controller);
+        }
+      },
+      itemBuilder: (BuildContext context) => <PopupMenuItem<_MenuOptions>>[
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.doPostRequest,
+          child: Text('Post Request'),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _onDoPostRequest(PlatformWebViewController controller) async {
+    final LoadRequestParams params = LoadRequestParams(
+      uri: Uri.parse('https://httpbin.org/post'),
+      method: LoadRequestMethod.post,
+      headers: const <String, String>{
+        'foo': 'bar',
+        'Content-Type': 'text/plain'
+      },
+      body: Uint8List.fromList('Test Body'.codeUnits),
+    );
+    await controller.loadRequest(params);
   }
 }
