89b2f759e0a1b1f28a85839fa91c8d19f79af204
masaPokemon <masa.gongon2011@gmail.com>
2024-09-30 09:59:20 +0900

    Update todo_model.dart

--- lib/todo_model.dart
+++ lib/todo_model.dart
@@ -3,28 +3,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class TodoModel {
   String _docId;
   String _title;
-  Timestamp _createAt;
 
   TodoModel(
     this._docId,
     this._title,
-    this._createAt,
   );
 
   String get docId => _docId;
   String get title => _title;
-  Timestamp get createAt => _createAt;
 
   TodoModel.fromMap(map) {
     _docId = map.documentID;
     _title = map['title'];
-    _createAt = map['createAt'];
   }
 
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{};
     map['title'] = _title;
-    map['createAt'] = _createAt;
     return map;
   }
 }
