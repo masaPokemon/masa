9c6390302623e0984c908aa147d3818f9f096d37
masaPokemon <masa.gongon2011@gmail.com>
2024-10-01 08:36:37 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:ai_barcode_scanner/ai_barcode_scanner.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:qrcode_reader_web/qrcode_reader_web.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -12,81 +10,51 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  String barcode = 'Tap  to scan';
+class _MyHomePageState extends State<MyHomePage> {
+  final List<QRCodeCapture> list = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(' Scanner'),
+        title: const Text("Example App"),
       ),
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
-            ElevatedButton(
-              child: const Text('Scan Barcode'),
-              onPressed: () async {
-                await Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => AiBarcodeScanner(
-                      onDispose: () {
-                        /// This is called when the barcode scanner is disposed.
-                        /// You can write your own logic here.
-                        debugPrint("Barcode scanner disposed!");
-                      },
-                      hideGalleryButton: false,
-                      controller: MobileScannerController(
-                        detectionSpeed: DetectionSpeed.noDuplicates,
-                      ),
-                      onDetect: (BarcodeCapture capture) {
-                        /// The row string scanned barcode value
-                        final String? scannedValue =
-                            capture.barcodes.first.rawValue;
-                        debugPrint("Barcode scanned: $scannedValue");
-
-                        /// The `Uint8List` image is only available if `returnImage` is set to `true`.
-                        final Uint8List? image = capture.image;
-                        debugPrint("Barcode image: $image");
-
-                        /// row data of the barcode
-                        final Object? raw = capture.raw;
-                        debugPrint("Barcode raw: $raw");
-
-                        /// List of scanned barcodes if any
-                        final List<Barcode> barcodes = capture.barcodes;
-                        debugPrint("Barcode list: $barcodes");
-                      },
-                      validator: (value) {
-                        if (value.barcodes.isEmpty) {
-                          return false;
-                        }
-                        if (!(value.barcodes.first.rawValue
-                                ?.contains('flutter.dev') ??
-                            false)) {
-                          return false;
-                        }
-                        return true;
-                      },
-                    ),
-                  ),
-                );
-              },
+            QRCodeReaderSquareWidget(
+              onDetect: (QRCodeCapture capture) => setState(() => list.add(capture)),
+              size: 250,
             ),
-            Text(barcode),
+            QRCodeReaderTransparentWidget(
+              onDetect: (QRCodeCapture capture) => setState(() => list.add(capture)),
+              targetSize: 250,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (_, index) {
+                  return ListTile(title: Text(list[index].raw));
+                },
+              ),
+            ),
           ],
         ),
       ),
