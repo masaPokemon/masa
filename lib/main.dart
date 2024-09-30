06d2df79b522d2449f62d57eca8f000b612fa8db
masaPokemon <masa.gongon2011@gmail.com>
2024-09-30 10:09:00 +0900

    Update main.darts

--- lib/main.dart
+++ lib/main.dart
@@ -1,174 +1,103 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:how_many_minutes_left/todo_service.dart';
-import 'package:provider/provider.dart';
-import 'auth_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// 匿名ユーザーごとに処理を分ける
 void main() {
-  runApp(
-    // providerを複数使うときはMultiProviderを使う。
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthService.instance()),
-        ChangeNotifierProvider(create: (_) => TodoService()),
-      ],
-      child: MyApp(),
-    ),
-  );
-}
-
-// 本番かリリースかを判断するには bool.fromEnvironment('dart.vm.product')を使う。
-// よりわかりやすくするためにラップして使っている。
-bool isRelease() {
-  bool _bool;
-  bool.fromEnvironment('dart.vm.product') ? _bool = true : _bool = false;
-  return _bool;
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ポイントシステム',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SignProcess(),
+      home: PointSystemPage(),
     );
   }
 }
 
-class SignProcess extends StatelessWidget {
+class PointSystemPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, AuthService authService, _) {
-        // ログインの状態に応じて処理を遷移させる。
-        switch (authService.status) {
-          case Status.uninitialized:
-            print('uninitialized');
-            return Center(child: CircularProgressIndicator());
-          case Status.unauthenticated:
-          case Status.authenticating:
-            print('anonymously');
-            authService.signInAnonymously();
-            return Center(child: CircularProgressIndicator());
-          case Status.authenticated:
-            print("authenticated");
-            break; // DbProcess();へ進む
-
-        }
-        return DbProcess();
-      },
-    );
-  }
+  _PointSystemPageState createState() => _PointSystemPageState();
 }
 
-class DbProcess extends StatelessWidget {
+class _PointSystemPageState extends State<PointSystemPage> {
+  int _points = 0;
+
   @override
-  Widget build(BuildContext context) {
-    final authService = Provider.of<AuthService>(context);
-    final todoService = Provider.of<TodoService>(context);
-    // firestoreのデータはuidごとに分けているので、データの取得前にtodoServiceにuidを渡してあげる
-    todoService.uid = authService.user.uid;
-    // streamのデータ(firestore)のデータが変更される度に自動でリビルドしてくれる
-    return StreamBuilder<QuerySnapshot>(
-      // firestoreからデータを拾ってくる
-      stream: todoService.dataPath.orderBy("createAt").snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        }
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting: // データの取得まち
-            return CircularProgressIndicator();
-          default:
-            // streamからデータを取得できたので、使いやすい形にかえてあげる
-            todoService.init(snapshot.data.documents);
-            return ViewPage();
-        }
-      },
-    );
+  void initState() {
+    super.initState();
+    _loadPoints();
   }
-}
 
-class ViewPage extends StatelessWidget {
+  // ポイントをロード
+  _loadPoints() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _points = prefs.getInt('points') ?? 0;
+    });
+  }
+
+  // ポイントを保存
+  _savePoints() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('points', _points);
+  }
+
+  // ポイントを加算
+  void _addPoints() {
+    setState(() {
+      _points += 10;
+    });
+    _savePoints();
+  }
+
+  // ポイントを減算
+  void _subtractPoints() {
+    setState(() {
+      _points = _points > 0 ? _points - 10 : 0;
+    });
+    _savePoints();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final todoService = Provider.of<TodoService>(context);
-
     return Scaffold(
       appBar: AppBar(
-          title: Center(
-              child: Text(
-                  'firestoreの動作確認\n(${isRelease() ? 'リリース' : 'デバック'}モード)'))),
+        title: Text('ポイントシステム'),
+      ),
       body: Center(
-        child: ListView.builder(
-          itemCount: todoService.todo.length,
-          itemBuilder: (BuildContext context, int index) {
-            final _date = todoService.todo[index].createAt.toDate();
-            return ListTile(
-              title: Text(todoService.todo[index].title),
-              subtitle: Text(_date.toString()),
-              trailing: IconButton(
-                icon: Icon(Icons.delete, color: Colors.red),
-                onPressed: () {
-                  todoService.deleteDocument(todoService.todo[index].docId);
-                },
-              ),
-            );
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '現在のポイント:',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              '$_points',
+              style: TextStyle(fontSize: 48),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _addPoints,
+                  child: Text('ポイントを追加'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _subtractPoints,
+                  child: Text('ポイントを減算'),
+                ),
+              ],
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return NameInputDialog();
-            },
-          );
-        },
       ),
     );
   }
 }
-
-class NameInputDialog extends StatefulWidget {
-  @override
-  _NameInputDialogState createState() => _NameInputDialogState();
-}
-
-class _NameInputDialogState extends State<NameInputDialog> {
-  TextEditingController _nameController = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    final todoService = Provider.of<TodoService>(context);
-    return AlertDialog(
-      title: Text('予定を入力してください'),
-      content: TextField(
-        controller: _nameController,
-        autofocus: true,
-        decoration: InputDecoration(
-            border:
-                OutlineInputBorder(borderRadius: BorderRadius.circular(16))),
-      ),
-      actions: <Widget>[
-        FlatButton(
-          child: Text('クリア'),
-          onPressed: _nameController.clear,
-        ),
-        FlatButton(
-          child: Text('決定'),
-          onPressed: () {
-            todoService.addTitle(_nameController.text);
-            Navigator.pop(context);
-          },
-        ),
-      ],
-    );
-  }
-}
-
