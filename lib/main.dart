0f9c5d2756c91219fcf22aba2d4c613cb67f4fc5
masaPokemon <masa.gongon2011@gmail.com>
2024-10-02 10:59:47 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+class ScannerWidget extends StatefulWidget {
+  const ScannerWidget({super.key});
+
+  @override
+  State<ScannerWidget> createState() => _ScannerWidgetState();
+}
+
+class _ScannerWidgetState extends State<ScannerWidget>
+    with SingleTickerProviderStateMixin {
+  MobileScannerController controller = MobileScannerController();
+  final PointsManager _pointsManager = PointsManager();
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF66FF99),
+        title: const Text('スキャンしよう'),
+      ),
+      backgroundColor: Colors.black,
+      body: Builder(
+        builder: (context) {
+          return MobileScanner(
+            controller: controller,
+            fit: BoxFit.contain,
+            onDetect: (scandata) {
+              setState(() {
+                _pointsManager.addPoints(10);
+                controller.stop();
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return MyApp();
+                    },
+                  ),
+                );
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+
 
 class MyApp extends StatelessWidget {
   @override
