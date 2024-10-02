407975fe7c0f7f367a9aaf52c3defafba86f930a
masaPokemon <masa.gongon2011@gmail.com>
2024-10-02 10:45:13 +0900

    Update scanner.dart

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -30,7 +30,7 @@ class _ScannerWidgetState extends State<ScannerWidget>
             fit: BoxFit.contain,
             onDetect: (scandata) {
               setState(() {
-                point = 10;
+                _pointsManager.addPoints(10);
                 controller.stop();
                 Navigator.of(context).pushReplacement(
                   MaterialPageRoute(
